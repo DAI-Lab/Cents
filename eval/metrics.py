e828e477da24c8f603bf17890b3598e26d6cf7ed
Michael Fuest <fuest@dai-desk32.lids.mit.edu>
2024-07-19 16:30:50 -0400

    added metrics

--- eval/metrics.py
+++ eval/metrics.py
@@ -4,35 +4,6 @@ import numpy as np
 import pandas as pd
 
 
-def K(x_i: float, y_j: float, sigma: float) -> float:
-    """
-    Gaussian (RBF) kernel function.
-    """
-    return np.exp((-np.abs(x_i - y_j) ** 2) / (2 * (sigma**2)))
-
-
-def max_mean_discrepancy(X: np.ndarray, Y: np.ndarray, sigma: float = 1.0) -> float:
-    """
-    Compute the Maximum Mean Discrepancy (MMD) between two time series.
-
-    Args:
-        X: Time series data 1.
-        Y: Time series data 2.
-        sigma: Bandwidth of the RBF kernel.
-
-    Returns:
-        The MMD value between the two time series.
-    """
-    N = len(X)
-    M = len(Y)
-
-    term_1 = sum([sum([K(x_i, x_j, sigma) for x_j in X]) for x_i in X])
-    term_2 = sum([sum([K(x_i, y_j, sigma) for y_j in Y]) for x_i in X])
-    term_3 = sum([sum([K(y_i, y_j, sigma) for y_j in Y]) for y_i in Y])
-
-    return ((1 / N**2) * term_1 - (2 / (M * N)) * term_2 + (1 / M**2) * term_3) ** 0.5
-
-
 def dynamic_time_warping_dist(
     X: np.ndarray, Y: np.ndarray, norm: Callable[[float], float] = np.linalg.norm
 ) -> Tuple[np.ndarray, float]:
@@ -66,3 +37,34 @@ def get_period_bounds(
     df: pd.DataFrame, timeseries_colname: str, month: int, weekday: int
 ) -> Tuple[float, float]:
     df = df.loc[(df["month"] == month) & (df["weekday"] == weekday)].copy()
+    array_timeseries = np.array(df[timeseries_colname].to_list())
+    min_values = np.min(array_timeseries, axis=0)
+    max_values = np.max(array_timeseries, axis=0)
+    return min_values, max_values
+
+
+def calculate_period_bound_mse(
+    timeseries: np.ndarray,
+    df: pd.DataFrame,
+    timeseries_colname: str,
+    month: int,
+    weekday: int,
+) -> float:
+    min_bounds, max_bounds = get_period_bounds(df, timeseries_colname, month, weekday)
+    mse = 0.0
+    for i, value in enumerate(timeseries):
+        if value < min_bounds[i]:
+            mse += (value - min_bounds[i]) ** 2
+        elif value > max_bounds[i]:
+            mse += (value - max_bounds[i]) ** 2
+
+    mse /= len(timeseries)
+    return mse
+
+
+def calculate_context_fid():
+    pass
+
+
+def calculate_wasserstein_distance():
+    pass
