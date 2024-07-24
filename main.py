86cf77c241e4822df1c760bef6d327a82fe76955
Michael Fuest <fuest@dai-desk32.lids.mit.edu>
2024-07-24 16:29:10 -0400

    updated main.py

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ import numpy as np
 import pandas as pd
 
 from data_utils.dataset import PecanStreetDataset, prepare_dataloader, split_dataset
+from eval.evaluator import Evaluator
 from generator.acgan import ACGAN
 
 
 def main():
-    data = PecanStreetDataset(
-        normalize=True, user_id=661, include_generation=True, threshold=(-2, 2)
+    full_dataset = PecanStreetDataset(
+        normalize=True, user_id=None, include_generation=True, threshold=(-2, 2)
     )
-    og_data = PecanStreetDataset(
-        normalize=False, user_id=661, include_generation=True, threshold=(-2, 2)
-    ).data
+    all_users = full_dataset.data.dataid.unique()
 
-    inv_data = data.inverse_transform(data.data)
-    a = 1
-    # train_dataset, val_dataset = split_dataset(data)
-    # model = ACGAN(
-    #     input_dim=1,
-    #     noise_dim=512,
-    #     embedding_dim=512,
-    #     window_length=96,
-    #     learning_rate=1e-4,
-    #     weight_path="runs/",
-    # )
+    for user in all_users:
+        data = PecanStreetDataset(
+            normalize=True, user_id=user, include_generation=True, threshold=(-2, 2)
+        )
+        train_dataset, val_dataset = split_dataset(data)
+        model = ACGAN(
+            input_dim=2,
+            noise_dim=512,
+            embedding_dim=512,
+            window_length=96,
+            learning_rate=1e-4,
+            weight_path="runs/",
+        )
+        model.train(train_dataset, val_dataset, batch_size=32, num_epoch=50)
+        user_evaluator = Evaluator(data.data, model, 2, "runs/")
+        user_evaluator.evaluate_for_user(user)
 
 
 if __name__ == "__main__":
