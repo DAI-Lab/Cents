3bed4fadc1dab4652d6520113352c76e68f2a02f
Michael Fuest <fuest@dai-desk32.lids.mit.edu>
2024-09-18 15:21:08 -0400

    pre-commits

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 
 """The setup script."""
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
